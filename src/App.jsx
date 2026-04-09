f1bb7826602e5b0c94ba12a2f757ecb2a8c7eeb7
Electorbas <electorbas@gmail.com>
2026-04-09 08:46:15 -0700

    Initialized Git

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Hero from "./Components/Hero"
+import Navbar from "./Components/Navbar"
+
+const App = () => {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+    </main>
+  )
+}
+
+export default App 
